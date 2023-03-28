6d26a1915f202be2d2de9aa047e68d1ec2666357
alexandre artus <artusalexandre@yahoo.fr>
2023-03-28 16:54:51 +0200

    Controller restaurant ok

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,7 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+Restaurant.destroy_all
+
+Restaurant.create!(name: "La Mère Brazier", address: "Rue de la Mère Brazier 1, 1000 Bruxelles", phone_number: "06 13 62 01 17", category: "french")
+Restaurant.create!(name: "Chez Gaston", address: "Rue des capucins 20, 69001 Lyon", phone_number: "06 13 62 01 17", category: "italian")
+Restaurant.create!(name: "Anatole", address: "Allée des Hautprès 26, 69001 Lyon", phone_number: "06 13 62 01 17", category: "chinese")
+Restaurant.create!(name: "La Charette", address: "Chemin de Fessy 175, 69001 Lyon", phone_number: "06 13 62 01 17", category: "french")
+Restaurant.create!(name: "La Mascotte de l'Océan", address: "Rue de carcan plage, 64000 Carcan", phone_number: "06 13 62 01 17", category: "french")
